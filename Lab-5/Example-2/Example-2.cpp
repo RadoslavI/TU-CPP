47abad237a65977723a1da44f24d26ea84c0c484
RadoslavI <100360507+RadoslavI@users.noreply.github.com>
2022-11-16 11:41:15 +0200

    added the first exercise

--- Lab-5/Example-2/Example-2.cpp
+++ Lab-5/Example-2/Example-2.cpp
@@ -1,20 +1,49 @@
-// Example-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
-
+class area {
+	double dim1, dim2; //размери на фигурата
+public:
+	void setarea(double d1, double d2)
+	{
+		dim1 = d1;
+		dim2 = d2;
+	}
+	void getdim(double& d1, double& d2)
+	{
+		d1 = dim1;
+		d2 = dim2;
+	}
+	virtual double getarea() = 0; //чисто виртуална функция
+};
+class rectangle : public area {
+public:
+	double getarea()
+	{
+		double d1, d2;
+		getdim(d1, d2);
+		return d1 * d2;
+	}
+};
+class triangle : public area {
+public:
+	double getarea()
+	{
+		double d1, d2;
+		getdim(d1, d2);
+		return 0.5 * d1 * d2;
+	}
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+	area* p;
+	rectangle r;
+	triangle t;
+	r.setarea(3.3, 4.5);
+	t.setarea(4.0, 5.0);
+	p = &r;
+	cout << "Rectangle has area : " << p->getarea() << '\n';
+	p = &t;
+	cout << "Triangle has area : " << p->getarea() << '\n';
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
